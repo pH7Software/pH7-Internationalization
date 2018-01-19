9f11269c0221cd69bdd9b76181debabe8b018d5b
Pierre-Henry Soria <hi@ph7.me>
2018-01-19 20:08:27 +0000

    Bumped year

--- pH7CMSLangs.sql
+++ pH7CMSLangs.sql
@@ -3,7 +3,7 @@
 -- Title:         SQL Internationalization package
 --
 -- Author:        Pierre-Henry Soria <hello@ph7cms.com>
--- Copyright:     (c) 2016-2017, Pierre-Henry Soria. All Rights Reserved.
+-- Copyright:     (c) 2016-2018, Pierre-Henry Soria. All Rights Reserved.
 -- License:       GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
 -- Package:       PH7 / Internationalize / Sql
 --
