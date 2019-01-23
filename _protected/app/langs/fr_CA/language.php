faad40eb75e7be0bc38c9b435d2d39f9bbb5608c
Pierre-Henry Soria <hi@ph7.me>
2019-01-23 18:46:53 +1300

    Add second @author tag
    instead of having two authors on the same line

--- _protected/app/langs/fr_CA/language.php
+++ _protected/app/langs/fr_CA/language.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * @author         Pierre-Henry Soria <ph7software@gmail.com> / Polyna-Maude R.-Summerside <devel@polynamaude.com>
+ * @author         Pierre-Henry Soria <ph7software@gmail.com>
+ * @author         Polyna-Maude R.-Summerside <devel@polynamaude.com>
  * @copyright      (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package        PH7 / App / Lang / Fr CA
